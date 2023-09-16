9a161fc89a81fd95c9c32bb2878905f39e8d4ebd
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-09-16 15:19:37 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,21 @@
+let score="33abc"
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33" -> 33
+// "33abc"->NaN
+// "true"-> 1
+let isLogged = "flase"
+let boolIs = Boolean(isLogged)
+console.log(typeof boolIs);
+console.log(boolIs);
+// "" -> gives false
+// 0 & 1 -> gives false and true
+// "xyz" -> true
+let someNo =""
+let stringNum= String(someNo)
+
+console.log(typeof stringNum );
+console.log(stringNum);
