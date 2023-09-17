adf8192c20d831a178f029917dbabc2aac17d25f
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-09-17 03:48:44 +0000

    operators

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -19,3 +19,21 @@ let stringNum= String(someNo)
 
 console.log(typeof stringNum );
 console.log(stringNum);
+
+// ..........   xxxxxxxxxxxxxxxxxxxxxx    ...................
+        // operations
+// let value = 3
+// let neg = -value   
+// console.log(neg);     
+// let str1 = "hello"
+// let str2=" vasu"
+// console.log(str1+str2);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
