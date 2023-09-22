1b5685b7f06f97054902cd883a6012939e10fb7e
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-09-22 14:19:18 +0000

    memories

---
+++ 01_basics/memories.js
@@ -0,0 +1,14 @@
+let myYoutube = "hsk"
+let anotherNm = myYoutube
+anotherNm = "chai"
+console.log(myYoutube);
+console.log(anotherNm);
+
+let userOne ={
+    user : "abc@gmail.com",
+    upi : "56782@ybl"
+}
+ let userTwo = userOne 
+ userTwo.email = "vasu910@gmail.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
---
+++ 01_basics/strings.js
@@ -0,0 +1,22 @@
+const name = "Vasudev"
+const repoCount = "31"
+// console.log(name+" has done "+repoCount+" repositories");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Codewar')
+console.log(gameName[0]);
+console.log(gameName[5]);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('w'));
+const another = gameName.slice(2, 5)
+console.log(another);
+const newString = "    vasudev"
+console.log(newString.trim());
+console.log(newString.trim());
+const url = "https://hitesh.com/%$erf"
+
+console.log(url.replace('%$er','-'));
+console.log(url.includes('.com'));
+const name1 = "vasu-hck-@ybl"
+console.log(name1.split('-'));
