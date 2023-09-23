2eaefc838a240d26847644352b9361c1374bedb9
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-09-23 18:21:15 +0000

    number and maths

---
+++ 01_basics/nums.js
@@ -0,0 +1,24 @@
+const score =400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().concat("fucks"));
+// console.log(balance.toFixed(3));
+
+const other = 123.89656
+// console.log(other.toPrecision(3));
+const num =10000000000
+// console.log(num.toLocaleString('en-IN'));
+
+// ...............xxxxxxxxxxxxxxxxxxxxxxxx..........
+console.log(Math.abs(-4));
+console.log(Math.round(7.99));
+console.log(Math.ceil(-4));
+console.log(Math.floor(-4));
+console.log(Math.min(2,3,6,15,78));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const max= 10
+const min = 20
+console.log(Math.floor((Math.random()*(max - min +1)+min)));
