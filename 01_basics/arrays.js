c338ebf1c353a09f6e41f7df4a326a319241a4aa
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-09-27 14:45:26 +0000

    array

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -1,7 +1,27 @@
 // slice and splice 
-let myArr=[1,2,3,4,5,]
-const mn1 = myArr.slice(1,4)
-const mn2 = myArr.splice(1,4)
-console.log("A ",myArr);
-console.log("B ",mn1);
-console.log("C ",mn2);
+// let myArr=[1,2,3,4,5,]
+// const mn1 = myArr.slice(1,4)
+// const mn2 = myArr.splice(1,4)
+// console.log("A ",myArr);
+// console.log("B ",mn1);
+// console.log("C ",mn2);
+const marvel_heros = ["Thor","Ironman","Captain Marvel"]
+const dc_heros = ["Flash","Wonder woman","Superman"]
+// marvel_heros.push(dc_heros) //here dc_heros becomes a single element
+// const allheros = marvel_heros.concat(dc_heros)
+// console.log(allheros);
+const all_new_heros =[...dc_heros,...marvel_heros]
+//console.log(all_new_heros);
+const new_array = [2,3,4,[3,6,7],7,[3,[4,2,1]]]
+const real_array = new_array.flat(Infinity)
+// console.log(real_array);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+
+console.log(Array.from({name:"hitesh"})); // intresting case for interview becs if u dont specify which one to form array keys or values; it returns empty array 
+
+ let score1 =100
+ let score2 =200
+ let score3 =300
+console.log(Array.of(score1, score2 , score3));// converts a set of variables as array
---
+++ 01_basics/objects.js
@@ -0,0 +1,19 @@
+    // object literals
+
+const jsUser = {
+    name: "Vasudev",
+    "full name":"Vasudev Shastri",// u cant access this with dot operator except square notaion
+    age : 18 ,
+    location : "Mumbai" ,
+    email : "vasudev@gmail.com",
+    isLoggedIn:false,
+    lastDay : ["Mon", "Tues"]
+}
+
+console.log(jsUser.email);
+console.log(jsUser["lastDay"]); // the input value should be in form of string
+
+
+
+
+
