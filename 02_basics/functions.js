b7d79e671c63765d571701fc9d60aeba442c737b
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-10-02 11:36:47 +0000

    functions2

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -37,3 +37,42 @@ console.log(loginUser("Vasudev"))
 console.log(loginUser(""))
 // if we dont pass any value -. it gives undefined
 console.log(loginUser())
+
+
+function calculateCartPrice(...num1){ // (...) -> rest operator
+    return num1
+}
+// for passing many values
+
+//console.log(calculateCartPrice(200,300,400)); // returns in array
+
+function calculateCartPrice(val1, val2,...num1){ 
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,500));// val1 =200 ; val2 =300 ; num1 = [400,500] ie. array
+
+
+const user = {
+    username :"hitesh",
+    price :1990 
+}
+
+function handleObject(anyObject){
+    console.log(`user name is ${anyObject.username} and price for your commodity is ${anyObject.price}`);
+}
+// handleObject(user)
+
+handleObject({
+    username:"Sammy",
+    price : 399
+})
+
+// taking array as input
+const myNew = [200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[2];
+}
+
+console.log(returnSecondValue(myNew));
+ 
