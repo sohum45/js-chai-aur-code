d1984c616f36e4bdd4eeaf10c11b35206e22bd16
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-10-06 17:26:38 +0000

    arrow

--- 02_basics/arrow.js
+++ 02_basics/arrow.js
@@ -14,5 +14,46 @@ const user ={
 console.log(this); // current context is empty because in node enviroment this refers to empty
 
 
+// function chai(){
+//     let username = "vasudev"
+//     console.log(this.username); // u cant use this inside functions
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () => {  // => this is a arrow function
+    let username = "vasudev"
+    console.log(this.username );
+}
+
+// arrow functions 
+
+// const addTwo =(num1,num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4));
+
+
+//    implicit return type declaration of arrow 
+//  const addTwo =(num1,num2) =>  (num1 + num2)  // for single line statement 
+//  console.log(addTwo(3,4));
+
+// object return 
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+ 
+
+
+
+
+
+
 
 
