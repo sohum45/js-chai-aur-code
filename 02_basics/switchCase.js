6b750fcbe2d238c02e2f9c9a9429af7cf54753d0
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-10-12 15:13:09 +0000

    switch

---
+++ 02_basics/switchCase.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+            console.log("March");
+            break;
+    case 3:
+                console.log("April");
+                break;
+    case 4:
+            console.log("May");
+            break;
+    case 5:
+            console.log("June");
+            break;
+    case 6:
+            console.log("July");
+            break;    
+    case 7:
+            console.log("August");
+            break;    
+    case 8:
+            console.log("Septmember");
+            break;    
+    default:
+        console.log("case not matched");
+        break;
+}
+
+
+
+
+
