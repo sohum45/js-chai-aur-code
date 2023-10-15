63816fde2bb4c33ab406da94cdf5f1e50d1b4db7
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-10-15 02:53:11 +0000

    truthy

--- 02_basics/truthyFalsy.js
+++ 02_basics/truthyFalsy.js
@@ -1,17 +1,25 @@
-const userEmail = "h@itesh.ai"
+// const userEmail = "h@itesh.ai"
 
-if(userEmail){
-    console.log("got user email");
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("Dont have user the email");
+// }
+// const userEmail =[]
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+
+// checking empty objects
+
+const emptyObj ={}
+if (Object.keys(emptyObj).length === 0){ // call objects and check the keys
+        console.log("object is empty");                    // at the end array of keys is formed
 }
-else{
-    console.log("Dont have user the email");
-}
 
-
-
-
-
-
-
-
+let var1;
+var1 = null ?? 100?? 30
+console.log(var1);
 
