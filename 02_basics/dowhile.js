d757a66d63b014591416adac689086dc0dd156b8
sohum45 <142403572+sohum45@users.noreply.github.com>
2023-10-17 01:47:41 +0000

    dowhile loop

---
+++ 02_basics/dowhile.js
@@ -0,0 +1,21 @@
+
+let index =0
+
+while (index<=10) {
+    // console.log(`Value of index ${index}`);
+    index +=2
+}
+let array1 = ["flash ","batman","spiderman","thor"]
+let arr = 0
+while(arr< array1.length){
+    //console.log(`Value is ${array1[arr]}`);
+    arr = arr+1
+}
+
+
+let score =11  // initialisation
+
+do{
+    console.log(`Score is ${score}`); // kaam phele condition baad me
+    score++
+} while(score <=10);
