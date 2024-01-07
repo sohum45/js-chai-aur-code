98a59bc0286292b422775efe4a5597e6eca0d7db
sohum45 <142403572+sohum45@users.noreply.github.com>
2024-01-07 03:33:13 +0000

    promise

---
+++ promise/promise.js
@@ -0,0 +1,70 @@
+const promiseOne = new Promise(function(resolve , reject){
+    // Do an async task
+    // DataBase calls
+    // cryptographic related
+    // network related
+    setTimeout(function (){
+        console.log("Async task is complete");
+        resolve()  // connecting resolve with .then
+    },3000)
+})
+
+// comsumption of promise
+
+promiseOne.then(function () {
+    console.log("Promised Consumed");
+})
+
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("task 2 is complete");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("Async for 2 is resolved");
+})
+
+const promise3 = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({
+            usernmae:"Vasudev Shastri",
+            mail:"vasu@gmail.com",
+            age:19
+        })
+    },1000)
+})
+
+promise3.then(function (user){
+    console.log(user);
+})
+
+
+const promise4 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({
+                username:"vasudev" ,
+                password: "1234$$"
+            })
+        }
+            else{
+                reject('ERROR:Something went wrong')
+            }
+
+    },1000)
+})
+
+promise4.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function (error){
+    console.log(error);
+})
+
+
+
+
