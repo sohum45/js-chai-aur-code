d036e5b6eabc04d53adb3cf73e3377b262812b3d
sohum45 <142403572+sohum45@users.noreply.github.com>
2024-02-07 16:26:55 +0000

    imp

--- promise/mathPi.js
+++ promise/mathPi.js
@@ -1,1 +1,36 @@
- 
+console.log(Math.PI);
+Math.Pi = 6
+console.log(Math.PI); // The value canot be overridden
+
+
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(descripter);
+
+// setting your own hard code checks on objects like Math.PI
+const chai = {
+    name:'ginger chai',
+    price:250,
+    isAvailable:true,
+
+    orderChai: function (){
+        console.log("Chai fat gyi");
+    }
+}
+console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai, "isAvailable"));
+Object.defineProperty(chai, 'isAvailable',{
+    writable:false,
+    enumerable:false
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "isAvailable"));
+
+
+// using for off loop
+for (const [key , value] of Object.entries(chai)) {  // iterator having properties
+    if(typeof value !== 'function'){
+        console.log(`${key} , ${value}`);
+    }
+}
+
+
+
